6cf44ece2791c206087ff08083894c5318b1d360
Rian <rian.r.silva8@aluno.senai.br>
2025-08-14 11:13:28 -0300

    commit -m "Alteração e adição"

--- resto_da_divisão.php
+++ resto_da_divisão.php
@@ -3,6 +3,7 @@
     $numero2 = 3;
     $resto = $numero1 % $numero2;
     $divisao = $numero1 / $numero2;
+    $numero_arredondado = round($divisao, 2);
 
-    echo "A divisão entre ". $numero1. " e ". $numero2. " é ". $divisao. " e seu resto é ". $resto;
+    echo "A divisão entre ". $numero1. " e ". $numero2. " é ". $numero_arredondado. " e seu resto é ". $resto;
 ?>
