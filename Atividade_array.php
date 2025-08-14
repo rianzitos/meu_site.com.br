0d3bcd5595fbb4eafe75c080f05140a7b389f540
Rian <rian.r.silva8@aluno.senai.br>
2025-08-14 15:47:26 -0300

    Adição atividade arrays

---
+++ Atividade_array.php
@@ -0,0 +1,7 @@
+<?php
+    $frutas = ["Maçã", "Banana", "Laranja", "Limão", "Pera", "Morango", "Rodrigo Garro"];
+    for($i = 0; $i < count($frutas); $i++){
+        echo $frutas[$i]."<br>";
+    } 
+
+?>
