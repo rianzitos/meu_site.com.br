d3563801cf50b50908686b41e0759d44b24941bf
Rian <rian.r.silva8@aluno.senai.br>
2025-08-21 15:56:51 -0300

    alteração

--- while.php
+++ while.php
@@ -6,10 +6,14 @@
             echo "Número: $contador<br>";
             $contador++;
         } 
+?>
 
-$contador = 1;
-do {
-    echo "Número: $contador<br>";
-    $contador++;
-}while ($contador <=5);
+<h3> Este é um Do While </h3>
+
+<?php
+    $contador = 1;
+    do {
+        echo "Número: $contador<br>";
+        $contador++;
+    }while ($contador <=5);
 ?>
